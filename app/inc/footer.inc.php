a6f9d9a98e3a190ea7b526ca24291f4de6903f02
Sviatoslav <newmedia27@gmail.com>
2016-11-21 17:42:31 +0200

    Footer

---
+++ app/inc/footer.inc.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<div class="container">
+    <div class="row">
+        <nav class="col-md-12 hidden-xxs">
+            <ul class="clearfix">
+                <li><a href="#">О компании</a></li>
+                <li><a href="#">Каталог</a></li>
+                <li><a href="#">Доставка</a></li>
+                <li><a href="#">Наши магазины</a></li>
+                <li><a href="#">Оптовикам</a></li>
+                <li><a href="#">Контакты</a></li>
+            </ul>
+        </nav>
+        <div class="footer-content col-md-12 col-sm-12 col-xs-12 col-xxs-12">
+            <div class="shop-info col-md-4 col-sm-5 col-xs-12 col-xxs-12">
+                <p class="dark">&copy; 2007-2011 Интернет-супермаркет «<a href="#">Elwau.ru</a>»</p>
+                <p>Адрес: 105118, г.Москва, ул. Кирпичная, д. 22</p>
+                <p>Тел.: +7 (495) 540-55-11, 8 (800) 555-55-90</p>
+                <p>Email: info@elwau.ru</p>
+            </div>
+            <div class="col-md-4 col-sm-3 col-xs-6 col-xxs-12 mid">
+                <img src="frontend/img/footer-img.jpg" alt="">
+                <img src="frontend/img/footer-img.jpg" alt="">
+            </div>
+            <div class="design-author col-md-4 col-sm-4 col-xs-6 col-xxs-12">
+                <p>Дизайн сайта - <a href="#">Плюснин Никита</a></p>
+            </div>
+        </div>
+    </div>
+
+</div>
--- app/inc/foter.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: JART
- * Date: 21.11.2016
- * Time: 16:57
- */
